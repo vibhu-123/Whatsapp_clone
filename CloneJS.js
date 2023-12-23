c9c8cd1045ccf642377d7a986611a3f72785f933
Vaibhav kumar <131232769+vibhu-123@users.noreply.github.com>
2023-12-23 17:47:36 +0530

    added  basic whatsapp clone

---
+++ CloneJS.js
@@ -0,0 +1,180 @@
+var r=0;
+function bhejna()
+{
+    if(r==0)
+    
+    {   
+        var p=document.getElementById("bhej");
+        p.classList.add("visible");
+        var y=document.getElementById("send").value;
+    document.getElementById("bhej").value=y;
+        r++;
+    }
+
+    else if(r==1)
+    {
+        var p=document.getElementById("bhej1");
+        p.classList.add("visible");
+        var y=document.getElementById("send").value;
+    document.getElementById("bhej1").value=y;
+    r++;
+    }
+    else if(r==2)
+    {
+        var p=document.getElementById("bhej2");
+        p.classList.add("visible");
+        var y=document.getElementById("send").value;
+    document.getElementById("bhej2").value=y;
+    r++;
+    }
+    else if(r==3)
+    {
+        var p=document.getElementById("bhej3");
+        p.classList.add("visible");
+        var y=document.getElementById("send").value;
+    document.getElementById("bhej3").value=y;
+    r++;
+    }
+    else if(r==4)
+    {
+        var p=document.getElementById("bhej4");
+        p.classList.add("visible");
+        var y=document.getElementById("send").value;
+    document.getElementById("bhej4").value=y;
+    r++;
+    }
+    else if(r==5)
+    {
+        var p=document.getElementById("bhej5");
+        p.classList.add("visible");
+        var y=document.getElementById("send").value;
+    document.getElementById("bhej5").value=y;
+    r++;
+    }
+    else if(r==6)
+    {
+        var p=document.getElementById("bhej6");
+        p.classList.add("visible");
+        var y=document.getElementById("send").value;
+    document.getElementById("bhej6").value=y;
+    r++;
+    }
+    else if(r==7)
+    {
+        var p=document.getElementById("bhej7");
+        p.classList.add("visible");
+        var y=document.getElementById("send").value;
+    document.getElementById("bhej7").value=y;
+    r++;
+    }
+    else if(r==8)
+    {
+        var p=document.getElementById("bhej8");
+        p.classList.add("visible");
+        var y=document.getElementById("send").value;
+    document.getElementById("bhej8").value=y;
+    r++;
+    }
+    else if(r==9)
+    {
+        var p=document.getElementById("bhej9");
+        p.classList.add("visible");
+        var y=document.getElementById("send").value;
+    document.getElementById("bhej9").value=y;
+    r++;
+    }
+    else if(r==10)
+    {
+        var p=document.getElementById("bhej10");
+        p.classList.add("visible");
+        var y=document.getElementById("send").value;
+    document.getElementById("bhej10").value=y;
+    r++;
+    }
+}
+
+function new1()
+{
+    // open("http://127.0.0.1:5500/wattsapp/index.html","_self");
+    // document.getElementById("change_name").innerHTML="Tushar Yadav";
+    // document.getElementById("change_photo").src="https://i.pinimg.com/474x/3b/75/7f/3b757f815c429fef230ce1333d3f696c.jpg";
+}
+function pen()
+{
+    // document.getElementById("ab1").click();
+    document.getElementById("change_name").innerHTML="Tushar Yadav";
+    document.getElementById("change_photo").src="https://i.pinimg.com/474x/3b/75/7f/3b757f815c429fef230ce1333d3f696c.jpg";
+}
+function pen1()
+{
+    document.getElementById("change_name").innerHTML="Anushaka Sharma";
+    document.getElementById("change_photo").src="https://i.pinimg.com/originals/2b/93/fb/2b93fbacb0a85a9e59650bce7bc8f384.jpg"
+}
+function pen2()
+{
+    document.getElementById("change_name").innerText="Rohan Verma";
+    document.getElementById("change_photo").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjGyCrEl6wQH4sfJ3hsLU4e8V9xQty8u4qN4Fcf1WOHw&usqp=CAU&ec=48665699"
+}
+function pen3()
+{
+    document.getElementById("change_name").innerText="Kiara Kumari";
+    document.getElementById("change_photo").src="https://funylife.in/wp-content/uploads/2022/11/20221105_164725-1024x1024.jpg"
+}
+function pen4()
+{
+    document.getElementById("change_name").innerText="Priyana Chaudhery Singh";
+    document.getElementById("change_photo").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwtvrUSTE6mkWYblnAM_7QHpIVZpqB2P0kV3ddi4nRVA&usqp=CAU&ec=48665699"
+}
+function pen5()
+{
+    document.getElementById("change_name").innerText="Sana Mirza M.P";
+    document.getElementById("change_photo").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVYWl_8iBFslVBZUNtUzmWXDs0NGY-SIUnFDrTpMsNjA&usqp=CAU&ec=48665699"
+}
+function pen6()
+{
+    document.getElementById("change_name").innerText="Parents";
+    document.getElementById("change_photo").src="https://pratidin24ghanta.com/wp-content/uploads/2023/02/Radhe-Krishna-Ki-Photo-HD_after.jpg";
+}
+function pen7()
+{
+    document.getElementById("change_name").innerText="Mama ji";
+    document.getElementById("change_photo").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT3uSnu3Ici1ez0hS6LHqc90uOzWOU7rY3UUbP1taVSQ&usqp=CAU&ec=48665699"
+}
+function pen8()
+{
+    document.getElementById("change_name").innerText="johnpur vala mausa";
+    document.getElementById("change_photo").src="https://live.staticflickr.com/3030/2658283615_cab8af9a50_c.jpg"
+}
+function pen9()
+{
+    document.getElementById("change_name").innerText="Love";
+    document.getElementById("change_photo").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuqUrZZGM9_cL6LvTowNoXjwlyawe726_7NvxE79r3VA&usqp=CAU&ec=48665699&reload=on"
+}
+function pen10()
+{
+    document.getElementById("change_name").innerText="Ravi Shastri";
+    document.getElementById("change_photo").src="https://i.guim.co.uk/img/media/c5b63fbdee2e371cd3c4d70b482b45ce3500432e/0_223_4395_2637/master/4395.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=188ec718e7fbc628439a067afb56c4d9"
+}
+function pen11()
+{
+    document.getElementById("change_name").innerText="Urfi Javed";
+    document.getElementById("change_photo").src="https://www.oneindia.com/ph-big/2022/11/urfi-javed-photos-photos_166884392300.jpg";
+}
+
+
+
+function effective() {
+    let input = document.getElementById('inp').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('nm');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="inp-item";                 
+        }
+    }
+    
+}
